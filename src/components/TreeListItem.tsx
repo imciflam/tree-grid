0a43e0fd7f87698d66d7507022803487aee4cda3
Your Name <33329377+imciflam@users.noreply.github.com>
2020-02-17 09:31:11 +0300

    + button component

--- src/components/TreeListItem.tsx
+++ src/components/TreeListItem.tsx
@@ -2,11 +2,20 @@ import React, { Component } from "react"
 import "./TreeListItem.css"
 
 export class TreeListItem extends Component<any, any> {
+  public getChildren(name: string) {
+    alert(name)
+  }
+
   render() {
     console.log(this.props)
     return (
       <li className='tree-list__item'>
-        <p>{this.props.name}</p>
+        {this.props.name ? (
+          <button onClick={() => this.getChildren(this.props.name)}>></button>
+        ) : (
+          ""
+        )}
+        <span className='tree-list__text'>{this.props.name}</span>
       </li>
     )
   }

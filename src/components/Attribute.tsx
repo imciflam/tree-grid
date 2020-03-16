a1b3ce569f5e7fc08ff94293218866dadce47843
Your Name <33329377+imciflam@users.noreply.github.com>
2020-03-16 11:38:07 +0300

    Update Attribute.tsx

--- src/components/Attribute.tsx
+++ src/components/Attribute.tsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react"
 
 export class Attribute extends Component<any, any> {
+  renderCurrent(data: any) {
+    let result = []
+    if (data && data.length !== 0) {
+      for (let element of data) {
+        result.push(<div>{element._Description}</div>)
+      }
+    }
+    return result
+  }
   render() {
     console.log(this.props)
     return (
       <div>
         <h1>Attribute</h1>
         <div>content</div>
+        <div>{this.renderCurrent(this.props.data)}</div>
       </div>
     )
   }

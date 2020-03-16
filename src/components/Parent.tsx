8890592c61b8e38d8b7081d52c1664ff49304e08
Your Name <33329377+imciflam@users.noreply.github.com>
2020-03-16 13:04:08 +0300

    + onclick for parent

--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -1,11 +1,20 @@
 import React, { Component } from "react"
 
 export class Parent extends Component<any, any> {
+  onClick = (name: string) => {
+    let entity = localStorage.getItem(name)
+    if (!entity) {
+      // axios call
+    }
+  }
   render() {
     console.log(this.props)
     return (
       <div
         style={{ color: "blue", border: "1px solid black", padding: "10px" }}
+        onClick={() => {
+          this.onClick(this.props.data._Name)
+        }}
       >
         {this.props.data._Description}
       </div>

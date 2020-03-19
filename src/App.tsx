c4155e979d66b179523a02505c542e0b0813ce81
Vita <vitalia@myrambler.ru>
2020-03-19 10:55:16 +0300

    + 2 more exemplary entities

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,52 @@
-import React, { Component } from "react"
-import axios from "axios"
-import Attribute from "./components/Attribute"
-import Child from "./components/Child"
-import Parent from "./components/Parent"
+import React, { Component } from "react";
+import axios from "axios";
+import Attribute from "./components/Attribute";
+import Child from "./components/Child";
+import Parent from "./components/Parent";
 
 export class App extends Component<any, any> {
   constructor(props: any) {
-    super(props)
-    this.state = { data: [] }
+    super(props);
+    this.state = { data: [] };
   }
 
   renderCurrent = (data: any) => {
     if (data && data.length !== 0) {
-      let result = []
+      let result = [];
       for (let element in data) {
         switch (element) {
           case "Parent":
-            result.push(<Parent data={data[element]} />) // bracket notation, because it's variable
-            break
+            result.push(<Parent data={data[element]} />); // bracket notation, because it's variable
+            break;
           case "Child":
-            result.push(<Child data={data[element]} />)
-            break
+            result.push(<Child data={data[element]} />);
+            break;
           case "Attribute":
-            result.push(<Attribute data={data[element]} />)
-            break
+            result.push(<Attribute data={data[element]} />);
+            break;
           default:
-            result.push(<div>unknown</div>)
-            break
+            result.push(<div>unknown</div>);
+            break;
         }
       }
-      return result
+      return result;
     }
-  }
+  };
 
   public componentDidMount() {
-    axios.get("1.json").then(response => {
-      this.setState({ data: response.data.Entity.Fields })
+    axios.get("GENERIC_REPORT.json").then(response => {
+      this.setState({ data: response.data.Entity.Fields });
       localStorage.setItem(
         response.data.Entity._Name,
         JSON.stringify(response.data.Entity)
-      )
-    })
+      );
+    });
   }
   render() {
     return (
       <React.Fragment>{this.renderCurrent(this.state.data)}</React.Fragment>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -1,39 +1,39 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 export class Child extends Component<any, any> {
   onClick = (name: string) => {
-    let entity = localStorage.getItem(name)
+    let entity = localStorage.getItem(name);
     if (!entity) {
       // axios call
     }
-  }
+  };
 
   renderCurrent = (data: any) => {
-    let result = []
+    let result = [];
     if (data && data.length !== 0) {
       for (let element of data) {
         result.push(
           <div
             style={{ border: "1px solid black", padding: "10px" }}
             onClick={() => {
-              this.onClick(element._Name)
+              this.onClick(element._Name);
             }}
           >
-            {element._Description}
+            <i>{element._Description}</i>
           </div>
-        )
+        );
       }
     }
-    return result
-  }
+    return result;
+  };
   render() {
-    console.log(this.props)
+    console.log(this.props);
     return (
       <div>
         <div>{this.renderCurrent(this.props.data)}</div>
       </div>
-    )
+    );
   }
 }
 
-export default Child
+export default Child;

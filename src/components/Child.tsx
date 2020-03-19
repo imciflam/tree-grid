10d43aa8a8f8aa03189bf4ac7fb76c468307fd59
Vita <vitalia@myrambler.ru>
2020-03-19 11:14:32 +0300

    either using axios or localstorage

--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export class Child extends Component<any, any> {
   onClick = (name: string) => {
     let entity = localStorage.getItem(name);
     if (!entity) {
       // axios call
+      axios.get(`${name}.json`).then(response => {
+        console.log(response.data);
+        this.setState({ data: response.data.Entity.Fields });
+        localStorage.setItem(
+          response.data.Entity._Name,
+          JSON.stringify(response.data.Entity)
+        );
+      });
     }
   };
 
@@ -16,7 +25,7 @@ export class Child extends Component<any, any> {
           <div
             style={{ border: "1px solid black", padding: "10px" }}
             onClick={() => {
-              this.onClick(element._Name);
+              this.onClick(element._Type.substring(6, element._Type.length));
             }}
           >
             <i>{element._Description}</i>
